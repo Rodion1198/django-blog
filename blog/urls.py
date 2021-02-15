1b7a9c68af0a654dfce589be96bea3330fbe1315
Rodion1198 <rodka1198@gmail.com>
2021-02-15 21:06:02 +0200

    added category and category_list

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
-from .views import AddPostView, ArticleDetailView, DeletePostView, HomeView, UpdatePostView
+from .views import AddCategoryView, AddPostView, ArticleDetailView, DeletePostView, HomeView,\
+    UpdatePostView, category_list_view, category_view
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('add_category/', AddCategoryView.as_view(), name='add_category'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
     path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
+    path('category/<str:category>/', category_view, name='category'),
+    path('category-list/', category_list_view, name='category-list'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,9 @@
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
 from .forms import EditForm, PostForm
-from .models import Post
-
-
-# def home(request):
-#     return render(request, 'home.html', {})
+from .models import Category, Post
 
 
 class HomeView(generic.ListView):
@@ -14,24 +11,51 @@ class HomeView(generic.ListView):
     template_name = 'home.html'
     ordering = ['-post_date']
 
+    def get_context_data(self, *args, **kwargs):
+        category_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context['category_menu'] = category_menu
+        return context
+
+
+def category_list_view(request):
+    category_list_menu = Category.objects.all()
+    return render(request, 'category_list.html', {'category_list_menu': category_list_menu})
+
+
+def category_view(request, category):
+    category_posts = Post.objects.filter(category=category.replace('-', ' '))
+    return render(request, 'categories.html', {'category': category.title().replace('-', ' '),
+                                               'category_posts': category_posts})
+
 
 class ArticleDetailView(generic.DetailView):
     model = Post
     template_name = 'article_detail.html'
+
+    def get_context_data(self, *args, **kwargs):
+        category_menu = Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
+        context['category_menu'] = category_menu
+        return context
 
 
 class AddPostView(generic.CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
-    # fields = '__all__'
+
+
+class AddCategoryView(generic.CreateView):
+    model = Category
+    template_name = 'add_category.html'
+    fields = '__all__'
 
 
 class UpdatePostView(generic.UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update_post.html'
-    # fields = ['title', 'title_tag', 'body']
 
 
 class DeletePostView(generic.DeleteView):
