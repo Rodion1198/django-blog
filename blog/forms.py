7c61c8ca55304da37b820fb754a53a414b6555fb
Rodion1198 <rodka1198@gmail.com>
2021-02-16 17:53:56 +0200

    added the hidden author then he add a blog post

--- blog/forms.py
+++ blog/forms.py
@@ -14,7 +14,7 @@ class PostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'category': forms.Select(choices=category, attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type': 'hidden'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
@@ -27,6 +27,5 @@ class EditForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
