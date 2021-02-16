44f456f16e55fe5cbfdc8f138dee72df2a44eefd
Rodion1198 <rodka1198@gmail.com>
2021-02-16 17:55:01 +0200

    added likes field and func which the count the likes

--- blog/models.py
+++ blog/models.py
@@ -18,11 +18,15 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
-    title_tag = models.CharField(max_length=250, default="My blog")
-    author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
+    title_tag = models.CharField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(max_length=255, default='coding')
     body = models.TextField()
     post_date = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='blog_posts')
+
+    def count_likes(self):
+        return self.likes.count()
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
