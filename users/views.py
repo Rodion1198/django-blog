45e0d4417584db51e46cfc39992ac41e3dce584d
Rodion1198 <rodka1198@gmail.com>
2021-02-17 19:29:26 +0200

    added user edit profile and password change logic

--- users/views.py
+++ users/views.py
@@ -1,9 +1,31 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import PasswordChangeView
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
 
+from .forms import EditProfileForm, PasswordChangingForm, SingUpForm
+
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    success_url = reverse_lazy('password_success')
+
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
+
+
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SingUpForm
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
+
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self, queryset=None):
+        return self.request.user
