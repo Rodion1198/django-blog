53535916d6e45ac17d385535f522fb2372c04096
Rodion1198 <rodka1198@gmail.com>
2021-02-19 01:38:35 +0200

    update settings and main urls

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -129,12 +129,15 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
