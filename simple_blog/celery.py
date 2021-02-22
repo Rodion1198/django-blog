696ef204c57a2031677de42bf38efc1d86f9762c
Rodion1198 <rodka1198@gmail.com>
2021-02-23 00:49:10 +0200

    added celery scraping posts

--- simple_blog/celery.py
+++ simple_blog/celery.py
@@ -6,13 +6,14 @@ from celery.schedules import crontab
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_blog.settings')
 
 app = Celery('simple_blog')
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'scraping': {
-        'task': 'blog.tasks.add_posts_rss',
+    'scraping_rss_posts': {
+        'task': 'blog.tasks.add_post_rss',
         'schedule': crontab(hour='1-23/2'),
-    },
+    }
 }
